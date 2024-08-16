5ed7d52525501610a549689ddbec015f6c03a30d
Gábor Szekeres <mail.szekg@gmail.com>
2024-08-16 18:10:31 +0200

    Created fizzBuzz and leetSpeak

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,24 @@
 function fizzBuzz(num1, num2) {
-  //write your code here
+  let numbers = []
+
+  // ENSURE THE NUMBERS ARE ASCENDING BY FINDING THE LOWER AND HIGHER VALUES FROM NUM1 AND NUM 2
+
+  let startingNumber = Math.min(num1, num2) 
+  let endingNumber = Math.max(num1, num2)
+
+  //LOGIC TO MAKE THE ARRAY
+
+  for (let i = startingNumber; i <= endingNumber; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      numbers.push("fizzbuzz")
+    } else if (i % 3 === 0) {
+      numbers.push("fizz")
+    } else if (i % 5 === 0) {
+      numbers.push("buzz")
+    } else numbers.push(i)
+  }
+
+  return numbers
 }
 
 module.exports = fizzBuzz
--- leetSpeak.js
+++ leetSpeak.js
@@ -1,5 +1,30 @@
 function leetSpeak(text) {
-  //write your code here
+
+  //MAKE AN ARRAY FOR EACH CHARACTER IN TEXT TURNED INTO UPPERCASE
+
+  let characterArray = text.toUpperCase().split("")
+  
+  //MAP THE ARRAY AND RETURN REPLACEMENT CHARACTERS
+
+  let leetArray = characterArray.map(character => {
+    if (character === "O") return "0"
+    if (character === "I" || character === "L") return "1"
+    if (character === "Z") return "2"
+    if (character === "E") return "3"
+    if (character === "A") return "4"
+    if (character === "S") return "5"
+    if (character === "G") return "6"
+    if (character === "T") return "7"
+    if (character === "B") return "8"
+    return character
+  })
+  
+  //RETURN THE ARRAY FROM THE MAP AS A STRING
+
+  return leetArray.join("")
+
 }
+
+leetSpeak("elite boy")
 
 module.exports = leetSpeak
